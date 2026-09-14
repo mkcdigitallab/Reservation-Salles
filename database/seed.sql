c9f026a364f40668a763453bb8ae5def9fa2b2ec
Papa Cissé <papac8443@gmail.com>
2026-09-14 10:39:57 +0000

    chore(database): remove obsolete SQL seed

--- database/seed.sql
+++
@@ -1,8 +0,0 @@
-SET NAMES utf8mb4;
-
-INSERT INTO salles (nom, batiment, capacite, type, active) VALUES
-    ('Salle A101', 'Bâtiment A', 30, 'cours', TRUE),
-    ('Salle Informatique 1', 'Bâtiment B', 25, 'informatique', TRUE),
-    ('Laboratoire 1', 'Bâtiment C', 20, 'laboratoire', TRUE),
-    ('Amphithéâtre Central', 'Bâtiment A', 150, 'amphitheatre', TRUE),
-    ('Salle de Réunion', 'Bâtiment D', 12, 'reunion', TRUE);
